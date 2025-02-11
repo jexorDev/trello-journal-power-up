875c411b158ea1118e45b74b58cc041d5507f2f8
jexorDev <dustineseger@yahoo.com>
2025-02-10 22:53:55 -0600

    get board data

--- journal.js
+++ journal.js
@@ -1,6 +1,6 @@
 var t = window.TrelloPowerUp.iframe();
 
-return t.get('board', 'shared', 'myKey')
+t.get('board', 'shared', 'myKey')
 .then(function (data) {
   console.log(JSON.stringify(data, null, 2));
 });
