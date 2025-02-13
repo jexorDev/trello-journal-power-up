1a2a980f828041f1d7b2584e677dbb55d7031363
jexorDev <dustineseger@yahoo.com>
2025-02-13 13:57:46 -0600

    install and test dayjs

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 var cors = require('cors');
 var express = require('express');
 var app = express();
-
 app.use(cors({ origin: 'https://trello.com' }));
+
+const dayjs = require('dayjs')
+var utc = require("dayjs/plugin/utc");
+dayjs.extend(utc);
