235c45abf30fc32f5899eade6f381fe79329121b
jexorDev <dustineseger@yahoo.com>
2025-02-13 15:03:39 -0600

    test express api

--- server.js
+++ server.js
@@ -1,4 +1,10 @@
 var cors = require('cors');
 var express = require('express');
 var app = express();
+
+app.listen(5000);
 app.use(cors({ origin: 'https://trello.com' }));
+
+app.getCardComments("/", (req, res) => {
+    res.send('hiya');
+  })
